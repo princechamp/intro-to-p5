e006505c23125b0da72957241a66b5afeca7092b
Prince Onwuvuche <ponwuvuche@wlapcs.org>
2017-03-01 15:32:01 +0000

    first commit

---
+++ sketch.js
@@ -0,0 +1,22 @@
+function setup(){
+    createCanvas(600,400);
+    background(100,100,100);
+}
+
+function draw(){
+    fill(28,34,92);
+    rect(290,100,200,250);
+    fill(78,144,32);
+    rect(290,100,100,250);
+    
+    fill(52,73,32);
+    ellipse(50,250,80,80);
+    fill(124,73,209)
+    ellipse(190,200,80,80);
+    
+    
+    line(50,20,0,0);
+    line(70,10,2,0);
+    line(46,23,68,9);
+    line(82,71,4,88);
+}
